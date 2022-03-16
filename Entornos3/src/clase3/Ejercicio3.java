316527f87b4b8506cff565783d513d8e538e504b
angel <angel@correo.com>
2022-03-16 21:00:06 +0100

    Github1

--- Entornos3/src/clase3/Ejercicio3.java
+++ Entornos3/src/clase3/Ejercicio3.java
@@ -7,7 +7,7 @@ public class Ejercicio3 {
 		System.out.println("Hola uuu");
 		//uwu
 		System.out.println("wwwwwww");
-		System.out.println("subida conectada con github");
+		System.out.println("subida conectada con github1");
 	}
 
 }
